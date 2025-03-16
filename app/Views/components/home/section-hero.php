ed8970f5ff8d8c0af7ab1b0e07ac903409ebdda1
AJI RAMDANI - ARM <hilmankeren0936@gmail.com>
2025-03-17 00:11:22 +0700

    update

---
+++ app/Views/components/home/section-hero.php
@@ -0,0 +1,13 @@
+<!-- ==== container box === -->
+ <div class="container-fluid">
+    <!-- === component right === -->
+     <div class="d-flex align-items-center justify-content-center min-vh-100 text-center">
+        <!-- === component text judul === -->
+        <div class="content-text">
+            <h1 class="text-uppercase montserrat-bold fs-10">apotek kemenkes</h1>
+            <h6>to support health quickly, easily and with care.
+        Healthy steps start here</h6>
+        </div>
+       
+     </div>
+ </div>
--- app/Views/home.php
+++ app/Views/home.php
@@ -7,9 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
     <?= view('components/header') ?>
+    <link rel="stylesheet" href="<?= base_url('components/css/fontGlobal.css') ?>">
 </head>
 <body>
     <!-- === component navigation === -->
     <?= view('components/navigation-type-one') ?>
+    <!-- === end component navigation ==== -->
+
+    <!-- === component section hero === -->
+    <?= view('components/home/section-hero') ?>
 </body>
 </html>
