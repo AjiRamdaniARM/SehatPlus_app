5e3bb6043d742f8c30b8e7a9821db2dac751f8a6
AJI RAMDANI - ARM <hilmankeren0936@gmail.com>
2025-03-25 20:15:48 +0700

    update page hak

--- app/Views/auth/components/c-akses-user.php
+++ app/Views/auth/components/c-akses-user.php
@@ -1,24 +1,40 @@
-<div class="container">
-    <div class="flex-custom">
-        <!-- Kiri: Button -->
-        <div class="content-buttons">
-            <button class="btn btn-primary">Button 1</button>
-            <button class="btn btn-success">Button 2</button>
-            <button class="btn btn-warning">Button 3</button>
+<section class="container d-flex align-items-center min-vh-100">
+    <div class="row w-100">
+       <!-- === component all button akses === -->
+        <div class="col-lg-6 d-flex flex-column justify-content-center gap-5">
+            <button class="btn bg-blue-1 text-white d-flex align-items-center gap-2 p-5">
+                <i class="bi bi-person-badge"></i> Admin
+            </button>
+            <button class="btn bg-green-1 text-white d-flex align-items-center gap-2 p-5">
+                <i class="bi bi-cash-stack"></i> Kasir
+            </button>
+            <button class="btn btn-warning d-flex align-items-center gap-2 p-5">
+                <i class="bi bi-person-check"></i> Owner
+            </button>
         </div>
-        <!-- Kanan: Gambar -->
-        <div class="content-image">
-            <img class="img-responsive w-50" src="/components/image/png-illustrasi-akses.png" alt="Akses">
+        <!-- === component gambar === -->
+        <div class="col-lg-6 d-flex justify-content-center">
+            <img src="/components/image/png-illustrasi-akses.png" class=" w-75" alt="Hero Image">
         </div>
     </div>
-</div>
+</section>
 
-<!-- === style css internal === -->
+<!-- === kode style css internal === -->
  <style>
-      .flex-custom {
-            display: flex;
-            justify-content: center;  /* Posisi tengah horizontal */
-            align-items: center;      /* Posisi tengah vertikal */
-                  /* Tinggi 100% viewport */
-        }
+  .bg-green-1 {
+    background-color: #355B64;
+    transition: background-color 0.3s ease-in-out;
+    }
+    .bg-green-1:hover {
+        background-color: #2A474F; /* Warna lebih gelap saat hover */
+    }
+
+    .bg-blue-1 {
+        background-color: #205C9A;
+        transition: background-color 0.3s ease-in-out;
+    }
+    .bg-blue-1:hover {
+        background-color: #174678; /* Warna lebih gelap saat hover */
+    }
+
  </style>
--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -5,4 +5,5 @@
 <link href="https://cdn.jsdelivr.net/npm/fastbootstrap@2.2.0/dist/css/fastbootstrap.min.css" rel="stylesheet" integrity="sha256-V6lu+OdYNKTKTsVFBuQsyIlDiRWiOmtC8VQ8Lzdm2i4=" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-
+<!-- === link cdn icon boostrap === -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
